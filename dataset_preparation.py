5a4ea48d474451a34f6c90e07ff560a14d0f3d53
Katya Feldman <katyapinich@gmail.com>
2020-04-14 16:23:40 +0300

    Rewriting of the dataset preparation script
    Just makes a csv file of all speakers with relevant columns

--- dataset_preparation.py
+++ dataset_preparation.py
@@ -1,40 +1,47 @@
 import os
-from os import listdir
 import shutil
 import csv
+from pathlib import Path
 
-RAW_DATASET = "data/raw"
-OUTPUT_DIR = "data/clean_speech"
-INFO_FILENAME = "spkrinfo.csv"
+CSV_PATH = './data/speakers.csv'
 
-speaker_dirs = [directory_name for directory_name in listdir(RAW_DATASET) if directory_name != ".DS_Store"]
 
-speaker_to_audio_files_map = {}
-for speaker_id in speaker_dirs:
-    speaker_dir = os.path.join(RAW_DATASET, speaker_id)
-    speaker_to_audio_files_map[speaker_id] = [os.path.join(speaker_dir, filename) for filename in listdir(speaker_dir) if filename.endswith(".WAV")]
+def get_directories(parent_dir: Path) -> list:
+    return [child_entry for child_entry in parent_dir.iterdir() if child_entry.is_dir()]
 
-for speaker_id, audio_files in speaker_to_audio_files_map.items():
-    for audio_file in audio_files:
-        source = audio_file
-        destination = os.path.join(OUTPUT_DIR, f'{speaker_id}_{os.path.basename(audio_file)}')
-        dest = shutil.copyfile(source, destination)
 
-print(listdir(OUTPUT_DIR))
+def map_speaker_to_files(dataset_filename: str) -> dict:
+    dataset_path = Path(dataset_filename)
+    speaker_to_file_map = {}
 
-# Write the csv file for labeling
-speaker_files = [filename for filename in listdir(OUTPUT_DIR) if filename != ".DS_Store"]
-rows = [["ID", "Sex", "File"]]
-for speaker_file in speaker_files:
-    filename = os.path.splitext(speaker_file)[0]
-    speaker_sex = filename[0]
-    speaker_id = filename[1:5]
-    rows.append([speaker_id, speaker_sex, speaker_file])
+    for entry in get_directories(dataset_path):
+        for speaker_dir in get_directories(entry):
+            speaker_to_file_map[speaker_dir.name] = [speaker_dir.joinpath(filename) for filename in speaker_dir.iterdir() if filename.suffix == '.WAV']
 
-speaker_info_filepath = os.path.join(OUTPUT_DIR, INFO_FILENAME)
-with open(speaker_info_filepath, 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerows(rows)
+    return speaker_to_file_map
+
+
+def write_csv_file(csv_filename: str, speakers_map: dict):
+    rows = [["ID", "Sex", "File"]]
+    for speaker_dir in speakers_map:
+        speaker_sex = speaker_dir[0]
+        speaker_id = speaker_dir[1:]
+        for speaker_file in speakers_map[speaker_dir]:
+            rows.append([speaker_id, speaker_sex, speaker_file])
+
+    csv_path = Path(CSV_PATH)
+    with open(csv_path, 'a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(rows)
+
+dataset_filename = './data/full/TIMIT/TRAIN'
+speakers_map = map_speaker_to_files(dataset_filename)
+write_csv_file(CSV_PATH, speakers_map)
+
+dataset_filename = './data/full/TIMIT/TEST'
+speakers_map = map_speaker_to_files(dataset_filename)
+write_csv_file(CSV_PATH, speakers_map)
+
 
 
 
