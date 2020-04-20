eb40a943b14614608ed9d96fff6104ce13a48cf6
Katya Feldman <katyapinich@gmail.com>
2020-04-20 19:56:17 +0300

    Converting dataset for siamese network

--- audio_dataset.py
+++ audio_dataset.py
@@ -1,3 +1,4 @@
+import random
 from pathlib import Path
 
 import torch
@@ -8,19 +9,35 @@ import librosa
 
 
 class AudioDataset(Dataset):
-    def __init__(self, x: DataFrame, y: DataFrame, transform=None):
+    def __init__(self, x: DataFrame, y: DataFrame, classes: dict, transform=None):
         self.x = x
         self.y = y
+        self.classes = classes
+        self.dataset_by_class = self.group_dataset(x, y, classes)
         self.transform = transform
+
+    def group_dataset(self, x: DataFrame, y: DataFrame, classes: dict) -> dict:
+        dataset_by_class = []
+        for class_id in classes.keys():
+            dataset_by_class[class_id] = []
+
+        for i, class_id in enumerate(y):
+            dataset_by_class[class_id] = x[i]
+
+        return dataset_by_class
 
     def __len__(self):
         return len(self.x)
 
-    def __getitem__(self, idx):
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
+    def __getitem__(self, index):
+        if torch.is_tensor(index):
+            index = index.tolist()
 
-        filename = self.x.iloc[idx]
+        if index % 2 == 1:
+            label = 1
+            class_index = random.randint(0, len(self.classes) - 1)
+            file1 = self.dataset_by_class[class_index]
+        filename = self.x.iloc[index]
         audio_path = Path(filename)
         signal, sampling_rate = librosa.load(audio_path)
 
