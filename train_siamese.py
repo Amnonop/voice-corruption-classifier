97ebb1028a1df15637587aac1229481abb670a17
Amnon Ophir <ophiramnon@gmail.com>
2020-05-25 11:16:03 +0300

    Improved margin and lr

--- train_siamese.py
+++ train_siamese.py
@@ -34,13 +34,13 @@ def main():
     one_shot = data_loader.make_oneshot_task(20)
 
     model = Siamese()
-    criterion = ContrastiveLoss(margin=2.5)
-    optimizer = Adam(model.parameters(), lr=0.001)
+    criterion = ContrastiveLoss(margin=6)
+    optimizer = Adam(model.parameters(), lr=0.0005)
 
     evaluate_every = 10  # interval for evaluating on one-shot tasks
     loss_every = 20  # interval for printing loss (iterations)
     batch_size = 32
-    num_iterations = 2000
+    num_iterations = 1000
     N_way = 5  # how many classes for testing one-shot tasks>
     n_val = 50  # how many one-shot tasks to validate on?
     best = -1
